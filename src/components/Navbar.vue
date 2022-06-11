71a42b9fa3068d0afa8f72310df46f4f67260cfe
Aulia Rahman <75147892+rahman1005@users.noreply.github.com>
2022-06-11 19:23:06 +0700

    ubah router link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
    <nav>
             <ul>
-                <li><a href="/">Home</a></li>
+                <li><router-link class="routerlink" to="/detailevent">Home</router-link></li>
                 <li><a href="/allevents">All Event</a></li>
                 <li><a href="/about">About Us</a></li>
                  <li><a href="/login">Buat Event</a></li>
